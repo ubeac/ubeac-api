74da098e5f07420a3d7ed3029ef5f74f62ed7380
Hesam Asnaashari <ilhesam@outlook.com>
2022-01-18 19:19:26 +0330

    Insert Unit

--- src/Identity/uBeac.Core.Identity/Services/Interfaces/IUnitService.cs
+++ src/Identity/uBeac.Core.Identity/Services/Interfaces/IUnitService.cs
@@ -4,6 +4,7 @@ public interface IUnitService<TKey, TUnit>
     where TKey : IEquatable<TKey>
     where TUnit : Unit<TKey>
 {
+    Task Insert(TUnit unit, CancellationToken cancellationToken = default);
 }
 
 public interface IUnitService<TUnit> : IUnitService<Guid, TUnit>
--- src/Identity/uBeac.Core.Identity/Services/UnitService.cs
+++ src/Identity/uBeac.Core.Identity/Services/UnitService.cs
@@ -4,9 +4,24 @@ public class UnitService<TKey, TUnit> : IUnitService<TKey, TUnit>
     where TKey : IEquatable<TKey>
     where TUnit : Unit<TKey>
 {
+    protected readonly IUnitRepository<TKey, TUnit> UnitRepository;
+
+    public UnitService(IUnitRepository<TKey, TUnit> unitRepository)
+    {
+        UnitRepository = unitRepository;
+    }
+
+    public async Task Insert(TUnit unit, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        await UnitRepository.Insert(unit, cancellationToken);
+    }
 }
 
 public class UnitService<TUnit> : UnitService<Guid, TUnit>, IUnitService<TUnit>
     where TUnit : Unit
 {
+    public UnitService(IUnitRepository<TUnit> unitRepository) : base(unitRepository)
+    {
+    }
 }
