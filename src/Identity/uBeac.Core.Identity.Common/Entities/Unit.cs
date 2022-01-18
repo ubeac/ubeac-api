48f0a4e9742fe34b9ad3a54ef1f11a37654b45a8
Hesam Asnaashari <ilhesam@outlook.com>
2022-01-18 18:36:16 +0330

    Add Unit entity

---
+++ src/Identity/uBeac.Core.Identity.Common/Entities/Unit.cs
@@ -0,0 +1,15 @@
+﻿namespace uBeac.Identity;
+
+public class Unit<TUnitKey> : IEntity<TUnitKey> where TUnitKey : IEquatable<TUnitKey>
+{
+    public virtual TUnitKey Id { get; set; }
+    public virtual string Name { get; set; }
+    public virtual string Code { get; set; }
+    public virtual string Type { get; set; }
+    public virtual string? Description { get; set; }
+    public virtual string? ParentUnitId { get; set; }
+}
+
+public class Unit : Unit<Guid>, IEntity
+{
+}
