588e423391b09f063dd372d55e27f07ffef14445
Hesam Asnaashari <ilhesam@outlook.com>
2022-01-19 04:14:18 +0330

    Correct name of MongoTokenRepository

---
+++ src/Identity/uBeac.Core.Identity.MongoDB/MongoUserTokenRepository.cs
@@ -0,0 +1,20 @@
+﻿using uBeac.Identity;
+using uBeac.Repositories.MongoDB;
+
+namespace uBeac.Core.Identity.MongoDB
+{
+    public class MongoUserTokenRepository<TUserKey> : MongoEntityRepository<TUserKey, UserToken<TUserKey>>, IUserTokenRepository<TUserKey>
+        where TUserKey : IEquatable<TUserKey>
+    {
+        public MongoUserTokenRepository(IMongoDBContext mongoDbContext) : base(mongoDbContext)
+        {
+        }
+    }
+
+    public class MongoUserTokenRepository : MongoUserTokenRepository<Guid>, IUserTokenRepository
+    {
+        public MongoUserTokenRepository(IMongoDBContext mongoDbContext) : base(mongoDbContext)
+        {
+        }
+    }
+}
