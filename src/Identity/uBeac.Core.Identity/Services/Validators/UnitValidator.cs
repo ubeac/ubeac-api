d0368bd999cf5f05a93d5976eb60b3527cb93b8f
Hesam Asnaashari <ilhesam@outlook.com>
2022-01-20 15:42:39 +0330

    Unit validator

--- src/Identity/uBeac.Core.Identity/Services/Validators/UnitValidator.cs
+++ src/Identity/uBeac.Core.Identity/Services/Validators/UnitValidator.cs
@@ -16,14 +16,17 @@ public class UnitValidator<TKey, TUnit> : IValidator<TUnit>
     public async Task<ValidationResult> ValidateAsync(TUnit obj)
     {
         var errors = new List<ValidationError>();
+        var identifiers = GetIdentifiers(obj);
 
         if (string.IsNullOrWhiteSpace(obj.Name)) errors.Add(new ValidationError(string.Empty, "Unit name is required."));
         if (string.IsNullOrWhiteSpace(obj.Code)) errors.Add(new ValidationError(string.Empty, "Unit code is required."));
         if (string.IsNullOrWhiteSpace(obj.Type)) errors.Add(new ValidationError(string.Empty, "Unit type is required."));
-        if (await Repository.Any(obj.Code, obj.Type)) errors.Add(new ValidationError(string.Empty, "Unit code/type is already exists."));
+        if (await Repository.Any(identifiers)) errors.Add(new ValidationError(string.Empty, "Unit code/type is already exists."));
 
         return errors.CreateResult();
     }
+
+    public UnitIdentifiers GetIdentifiers(TUnit unit) => unit.GetIdentifiers<TKey, TUnit>();
 }
 
 public class UnitValidator<TUnit> : UnitValidator<Guid, TUnit>
