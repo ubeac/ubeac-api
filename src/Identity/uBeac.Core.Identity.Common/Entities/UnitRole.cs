e5d3e0a2bf40083b298b63cc488bbf0086d0464b
Hesam Asnaashari <ilhesam@outlook.com>
2022-01-22 10:20:41 +0330

    Add unit role entity

---
+++ src/Identity/uBeac.Core.Identity.Common/Entities/UnitRole.cs
@@ -0,0 +1,24 @@
+﻿namespace uBeac.Identity;
+
+public class UnitRole<TUnitRoleKey> : IEntity<TUnitRoleKey> where TUnitRoleKey : IEquatable<TUnitRoleKey>
+{
+    public UnitRole(TUnitRoleKey id, string unitCode, string type, string role)
+    {
+        Id = id;
+        UnitCode = unitCode;
+        Type = type;
+        Role = role;
+    }
+
+    public virtual TUnitRoleKey Id { get; set; }
+    [Identifier] public virtual string UnitCode { get; set; }
+    [Identifier] public virtual string Type { get; set; }
+    [Identifier] public virtual string Role { get; set; }
+}
+
+public class UnitRole : UnitRole<Guid>, IEntity
+{
+    public UnitRole(string unitCode, string type, string role) : base(Guid.NewGuid(), unitCode, type, role)
+    {
+    }
+}
