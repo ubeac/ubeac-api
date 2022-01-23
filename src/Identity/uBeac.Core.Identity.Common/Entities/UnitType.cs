15ae4ef39c8a3691d5e82b37bbbd8cb0d7f3d6b0
Amir Pournasserian <ap@momentaj.com>
2022-01-23 10:09:55 -0500

    UnitType model cleanup

--- src/Identity/uBeac.Core.Identity.Common/Entities/UnitType.cs
+++ src/Identity/uBeac.Core.Identity.Common/Entities/UnitType.cs
@@ -3,9 +3,9 @@
 public class UnitType<TUnitTypeKey> : IEntity<TUnitTypeKey> where TUnitTypeKey : IEquatable<TUnitTypeKey>
 {
     public virtual TUnitTypeKey Id { get; set; }
-    // TODO: do we really need the Identifier attribute?
-    [Identifier] public virtual string Code { get; set; }
+    public virtual string Code { get; set; }
     public virtual string Name { get; set; }
+    public virtual string Description { get; set; }
 }
 
 public class UnitType : UnitType<Guid>, IEntity
