ed79ed903d600aeb9e16c0dc2d7231527c8a5452
Hesam Asnaashari <ilhesam@outlook.com>
2022-02-11 22:25:50 +0330

    Separate repository extensions

---
+++ src/Identity/uBeac.Core.Identity.MongoDB/Extensions/RoleExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using uBeac.Identity;
+using uBeac.Identity.MongoDB;
+using uBeac.Repositories.MongoDB;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class RoleExtensions
+{
+    public static IServiceCollection AddMongoDBRoleRepository<TMongoDbContext, TRoleKey, TRole>(this IServiceCollection services)
+        where TMongoDbContext : class, IMongoDBContext
+        where TRoleKey : IEquatable<TRoleKey>
+        where TRole : Role<TRoleKey>
+    {
+        services.TryAddScoped<IRoleRepository<TRoleKey, TRole>>(provider =>
+        {
+            var dbContext = provider.GetRequiredService<TMongoDbContext>();
+            return new MongoRoleRepository<TRoleKey, TRole>(dbContext);
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddMongoDBRoleRepository<TMongoDbContext, TRole>(this IServiceCollection services)
+        where TMongoDbContext : class, IMongoDBContext
+        where TRole : Role
+    {
+        services.TryAddScoped<IRoleRepository<TRole>>(provider =>
+        {
+            var dbContext = provider.GetRequiredService<TMongoDbContext>();
+            return new MongoRoleRepository<TRole>(dbContext);
+        });
+
+        return services;
+    }
+}
---
+++ src/Identity/uBeac.Core.Identity.MongoDB/Extensions/UserTokenExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using uBeac.Identity;
+using uBeac.Identity.MongoDB;
+using uBeac.Repositories.MongoDB;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class UserTokenExtensions
+{
+    public static IServiceCollection AddMongoDBUserTokenRepository<TMongoDbContext, TUserKey>(this IServiceCollection services)
+        where TMongoDbContext : class, IMongoDBContext
+        where TUserKey : IEquatable<TUserKey>
+    {
+        services.TryAddScoped<IUserTokenRepository<TUserKey>>(provider =>
+        {
+            var dbContext = provider.GetRequiredService<TMongoDbContext>();
+            return new MongoUserTokenRepository<TUserKey>(dbContext);
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddMongoDBUserTokenRepository<TMongoDbContext>(this IServiceCollection services)
+        where TMongoDbContext : class, IMongoDBContext
+    {
+        services.TryAddScoped<IUserTokenRepository>(provider =>
+        {
+            var dbContext = provider.GetRequiredService<TMongoDbContext>();
+            return new MongoUserTokenRepository(dbContext);
+        });
+
+        return services;
+    }
+}
