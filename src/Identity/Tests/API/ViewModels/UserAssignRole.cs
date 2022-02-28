2acd2f33ebfdbf4a61dc7db33030950b6fb7ae8d
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-02-28 04:52:31 +0330

    Implement get role by id

---
+++ src/Identity/Tests/API/ViewModels/UserAssignRole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace API;
+
+public class AssignRoleRequest<TKey> where TKey : IEquatable<TKey>
+{
+    [Required]
+    public virtual TKey Id { get; set; }
+
+    public List<string> Roles { get; set; }
+}
+
+public class AssignRoleRequest : AssignRoleRequest<Guid>
+{
+}
