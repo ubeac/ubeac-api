9c98196838b8eb3be4633c4d3ef1bf87fef6111b
Amir Pournasserian <ap@momentaj.com>
2022-03-24 10:05:08 -0400

    Extracting AuditEntity to new file

--- src/Common/uBeac.Core.Common/Entity.cs
+++ src/Common/uBeac.Core.Common/Entity.cs
@@ -9,40 +9,11 @@ public interface IEntity : IEntity<Guid>
 {
 }
 
-public interface IAuditEntity<TKey> : IEntity<TKey> where TKey : IEquatable<TKey>
-{
-    string CreatedBy { get; set; } // UserName
-    string CreatedByIp { get; set; } // IpAddress
-    DateTime CreatedAt { get; set; }
-    string LastUpdatedBy { get; set; } // UserName
-    string LastUpdatedByIp { get; set; } // IpAddress
-    DateTime? LastUpdatedAt { get; set; }
-}
-
-public interface IAuditEntity : IAuditEntity<Guid>, IEntity
-{
-}
-
 public class Entity<TKey> : IEntity<TKey> where TKey : IEquatable<TKey>
 {
     public virtual TKey Id { get; set; }
 }
 
 public class Entity : Entity<Guid>, IEntity
-{
-}
-
-public class AuditEntity<TKey> : IAuditEntity<TKey> where TKey : IEquatable<TKey>
-{
-    public TKey Id { get; set; }
-    public string CreatedBy { get; set; }
-    public string CreatedByIp { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public string LastUpdatedBy { get; set; }
-    public string LastUpdatedByIp { get; set; }
-    public DateTime? LastUpdatedAt { get; set; }
-}
-
-public class AuditEntity : AuditEntity<Guid>, IAuditEntity
 {
 }
