8049d2c1d5ec52e563ee48b66083c8343e68d524
Amir Pournasserian <ap@momentaj.com>
2022-03-24 10:14:32 -0400

    Namespace formatting in MongoDB repository

--- src/Repository/uBeac.Core.Repositories.MongoDB/MongoDBContext.cs
+++ src/Repository/uBeac.Core.Repositories.MongoDB/MongoDBContext.cs
@@ -1,29 +1,29 @@
 ﻿using MongoDB.Driver;
 
-namespace uBeac.Repositories.MongoDB
+namespace uBeac.Repositories.MongoDB;
+
+public interface IMongoDBContext
 {
-    public interface IMongoDBContext
-    {
-        IMongoDatabase Database { get; }
-    }
-
-    public class MongoDBContext : IMongoDBContext
-    {
-        public MongoDBContext(MongoDBOptions<MongoDBContext> options)
-        {
-            var mongoUrl = new MongoUrl(options.ConnectionString);
-            var client = new MongoClient(mongoUrl);
-            try
-            {
-                if (options.DropExistDatabase) client.DropDatabase(mongoUrl.DatabaseName);
-            }
-            catch
-            {
-                // ignored
-            }
-            Database = client.GetDatabase(mongoUrl.DatabaseName);
-        }
-
-        public IMongoDatabase Database { get; }
-    }
+    IMongoDatabase Database { get; }
 }
+
+public class MongoDBContext : IMongoDBContext
+{
+    public MongoDBContext(MongoDBOptions<MongoDBContext> options)
+    {
+        var mongoUrl = new MongoUrl(options.ConnectionString);
+        var client = new MongoClient(mongoUrl);
+        try
+        {
+            if (options.DropExistDatabase) client.DropDatabase(mongoUrl.DatabaseName);
+        }
+        catch
+        {
+            // ignored
+        }
+        Database = client.GetDatabase(mongoUrl.DatabaseName);
+    }
+
+    public IMongoDatabase Database { get; }
+}
+
--- src/Repository/uBeac.Core.Repositories.MongoDB/MongoDBServicesExtensions.cs
+++ src/Repository/uBeac.Core.Repositories.MongoDB/MongoDBServicesExtensions.cs
@@ -6,57 +6,56 @@ using MongoDB.Bson.Serialization.Serializers;
 using uBeac.Repositories;
 using uBeac.Repositories.MongoDB;
 
-namespace Microsoft.Extensions.DependencyInjection
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class MongoDBServicesExtensions
 {
-    public static class MongoDBServicesExtensions
+    public static IServiceCollection AddMongo<TMongoDbContext>(this IServiceCollection services, string connectionString, bool dropExistDatabase = false)
+       where TMongoDbContext : class, IMongoDBContext
     {
-        public static IServiceCollection AddMongo<TMongoDbContext>(this IServiceCollection services, string connectionString, bool dropExistDatabase = false)
-           where TMongoDbContext : class, IMongoDBContext
+        services.TryAddSingleton(provider =>
         {
-            services.TryAddSingleton(provider =>
-            {
-                var configuration = provider.GetService<IConfiguration>();
-                return new MongoDBOptions<TMongoDbContext>(configuration.GetConnectionString(connectionString), dropExistDatabase);
-            });
+            var configuration = provider.GetService<IConfiguration>();
+            return new MongoDBOptions<TMongoDbContext>(configuration.GetConnectionString(connectionString), dropExistDatabase);
+        });
 
-            services.TryAddSingleton<TMongoDbContext>();
-            services.TryAddSingleton<IMongoDBContext, TMongoDbContext>();
+        services.TryAddSingleton<TMongoDbContext>();
+        services.TryAddSingleton<IMongoDBContext, TMongoDbContext>();
 
-            return services;
-        }
+        return services;
+    }
 
-        public static IServiceCollection AddDefaultBsonSerializers(this IServiceCollection services)
-        {
+    public static IServiceCollection AddDefaultBsonSerializers(this IServiceCollection services)
+    {
 #pragma warning disable CS0618 // Type or member is obsolete
-            BsonDefaults.GuidRepresentationMode = GuidRepresentationMode.V3;
+        BsonDefaults.GuidRepresentationMode = GuidRepresentationMode.V3;
 #pragma warning restore CS0618 // Type or member is obsolete
 
-            try
-            {
-                BsonSerializer.RegisterSerializer(typeof(Guid), new GuidSerializer(GuidRepresentation.Standard));
-                BsonSerializer.RegisterSerializer(typeof(decimal), new DecimalSerializer(BsonType.Decimal128));
-                BsonSerializer.RegisterSerializer(typeof(decimal?), new NullableSerializer<decimal>(new DecimalSerializer(BsonType.Decimal128)));
-            }
-            catch (BsonSerializationException)
-            {
-            }
-
-            return services;
-        }
-
-        public static IServiceCollection AddRepository<TInterface, TImplementation>(this IServiceCollection services)
-            where TInterface : IRepository
-            where TImplementation : class, IRepository
+        try
         {
-            services.TryAddScoped(typeof(TInterface), typeof(TImplementation));
-            return services;
-
+            BsonSerializer.RegisterSerializer(typeof(Guid), new GuidSerializer(GuidRepresentation.Standard));
+            BsonSerializer.RegisterSerializer(typeof(decimal), new DecimalSerializer(BsonType.Decimal128));
+            BsonSerializer.RegisterSerializer(typeof(decimal?), new NullableSerializer<decimal>(new DecimalSerializer(BsonType.Decimal128)));
         }
-
-        public static IServiceCollection AddRepository(this IServiceCollection services, Type interfaceType, Type implementationType)
+        catch (BsonSerializationException)
         {
-            services.TryAddScoped(interfaceType, implementationType);
-            return services;
         }
+
+        return services;
+    }
+
+    public static IServiceCollection AddRepository<TInterface, TImplementation>(this IServiceCollection services)
+        where TInterface : IRepository
+        where TImplementation : class, IRepository
+    {
+        services.TryAddScoped(typeof(TInterface), typeof(TImplementation));
+        return services;
+
+    }
+
+    public static IServiceCollection AddRepository(this IServiceCollection services, Type interfaceType, Type implementationType)
+    {
+        services.TryAddScoped(interfaceType, implementationType);
+        return services;
     }
 }
