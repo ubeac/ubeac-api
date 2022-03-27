94cd06bc0d905f8d95b156db1768b0b5fc7ea4b6
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-03-27 22:39:49 +0430

    Add Debugger

---
+++ src/Web/uBeac.Core.Web.Common/Debugger.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace uBeac.Web;
+
+public class Debugger : IDebugger
+{
+    private readonly HttpContext _context;
+    private const string ITEMS_KEY = "internalDebug";
+
+    public Debugger(IHttpContextAccessor accessor)
+    {
+        _context = accessor.HttpContext;
+    }
+
+    public void Add(object value)
+    {
+        _context.Items[ITEMS_KEY] ??= new List<object>();
+        var debugList = _context.Items[ITEMS_KEY] as List<object>;
+        debugList!.Add(value);
+    }
+
+    public List<object> GetValues() => _context.Items[ITEMS_KEY] as List<object> ?? new List<object>();
+}
