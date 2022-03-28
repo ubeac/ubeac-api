3c0d6e34267de5777245adccf1b64664422b5ec3
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-03-29 00:30:47 +0430

    Initialize uBeac.Core.Identity.Auth

---
+++ src/Identity/uBeac.Core.Identity.Auth/IAuthService.cs
@@ -0,0 +1,8 @@
+﻿using System.Security.Claims;
+
+namespace uBeac.Auth;
+
+public interface IAuthService
+{
+    Task<IEnumerable<Claim>> ValidateToken(string accessToken);
+}
--- src/Identity/uBeac.Core.Identity/JwtOptions.cs
+++ src/Identity/uBeac.Core.Identity/JwtOptions.cs
@@ -1,11 +1,10 @@
-﻿namespace uBeac.Identity
+﻿namespace uBeac.Identity;
+
+public class JwtOptions
 {
-    public class JwtOptions
-    {
-        public string Issuer { get; set; } = string.Empty;
-        public string Audience { get; set; } = string.Empty;
-        public double TokenExpiry { get; set; } = -1; // second, -1 means never expire
-        public double RefreshTokenExpiry { get; set; } = -1; // second, -1 means never expire
-        public string Secret { get; set; } = string.Empty;
-    }
+    public string Issuer { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
+    public double TokenExpiry { get; set; } = -1; // second, -1 means never expire
+    public double RefreshTokenExpiry { get; set; } = -1; // second, -1 means never expire
+    public string Secret { get; set; } = string.Empty;
 }
