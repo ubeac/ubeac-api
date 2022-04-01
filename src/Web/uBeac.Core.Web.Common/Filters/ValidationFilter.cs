d8f5b7cbaf42b4ccd0c16b417cb4d265347642b9
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-04-01 15:56:05 +0430

    Fix action filters issue

--- src/Web/uBeac.Core.Web.Common/Filters/ValidationFilter.cs
+++ src/Web/uBeac.Core.Web.Common/Filters/ValidationFilter.cs
@@ -24,7 +24,6 @@ public class ValidationFilter : IAsyncActionFilter
                     result.Errors.Add(new Error { Code = key, Description = key + "," + error.ErrorMessage });
 
             context.Result = new ObjectResult(result);
-            context.HttpContext.Response.StatusCode = result.Code;
         }
     }
 }
