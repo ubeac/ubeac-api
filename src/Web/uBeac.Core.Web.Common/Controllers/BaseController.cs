68529980d21970c72819f3e2d366bdd5c96ab10f
Amir Pournasserian <ap@momentaj.com>
2022-04-09 17:05:20 -0400

    Exception handling issues in API are resolved

--- src/Web/uBeac.Core.Web.Common/Controllers/BaseController.cs
+++ src/Web/uBeac.Core.Web.Common/Controllers/BaseController.cs
@@ -5,7 +5,6 @@ namespace uBeac.Web;
 [Route("API/[controller]/[action]/")]
 [ApiController]
 [Produces("application/json")]
-[TypeFilter(typeof(ExceptionHandlingFilter))]
 [TypeFilter(typeof(ResultFilter), Order = 0)]
 [TypeFilter(typeof(ModelStateValidationFilter), Order = 1)]
 public abstract class BaseController
