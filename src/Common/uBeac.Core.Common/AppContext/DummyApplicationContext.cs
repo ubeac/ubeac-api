c745dfdd3c435adc03473315f2513f40269515f6
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-04-26 20:20:07 +0430

    Change type of user ip to string

--- src/Common/uBeac.Core.Common/AppContext/DummyApplicationContext.cs
+++ src/Common/uBeac.Core.Common/AppContext/DummyApplicationContext.cs
@@ -7,7 +7,7 @@ namespace uBeac
         public virtual string TraceId { get; } = Guid.NewGuid().ToString();
         public virtual string SessionId { get; } = string.Empty;
         public virtual string UserName { get; } = string.Empty;
-        public virtual IPAddress UserIp { get; } = IPAddress.Parse("127.0.1.1");
+        public virtual string UserIp { get; } = IPAddress.Parse("127.0.1.1").ToString();
         public virtual string Language { get; } = "en-US";
     }
 }
