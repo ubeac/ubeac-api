9a711d1d4df32be9756a0aea5a8a6bbe3854ea3d
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-04-27 20:12:01 +0430

    Add create, update and delete entity options to repository

---
+++ src/Repository/uBeac.Core.Repositories.Abstractions/Options.cs
@@ -0,0 +1,16 @@
+﻿namespace uBeac.Repositories;
+
+public class CreateEntityOptions
+{
+    public string? ActionName { get; set; }
+}
+
+public class UpdateEntityOptions
+{
+    public string? ActionName { get; set; }
+}
+
+public class DeleteEntityOptions
+{
+    public string? ActionName { get; set; }
+}
