20620eb19f4831ec9dc00dcc80fca689c450b6af
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-05-02 15:58:00 +0430

    Implement UID and SID

--- src/Web/uBeac.Core.Web.Common/ApplicationContext.cs
+++ src/Web/uBeac.Core.Web.Common/ApplicationContext.cs
@@ -6,20 +6,24 @@ namespace uBeac.Web;
 public class ApplicationContext : IApplicationContext
 {
     protected readonly IHttpContextAccessor Accessor;
+    protected const string SidHeaderKey = "SID";
+    protected const string UidHeaderKey = "UID";
 
     public ApplicationContext(IHttpContextAccessor accessor)
     {
         Accessor = accessor;
 
         TraceId = Accessor.HttpContext?.TraceIdentifier;
-        SessionId = Accessor.HttpContext?.TraceIdentifier;
+        UniqueId = Accessor.HttpContext?.Request?.Headers?.FirstOrDefault(_ => _.Key.Equals(UidHeaderKey, StringComparison.OrdinalIgnoreCase)).Value;
+        SessionId = Accessor.HttpContext?.Request?.Headers?.FirstOrDefault(_ => _.Key.Equals(SidHeaderKey, StringComparison.OrdinalIgnoreCase)).Value;
         UserName = Accessor.HttpContext?.User?.Identity?.Name;
         UserIp = Accessor.HttpContext?.Connection?.RemoteIpAddress;
         Language = Accessor.HttpContext?.Request?.GetTypedHeaders().AcceptLanguage.FirstOrDefault()?.Value.Value ?? "en-US";
     }
 
     public string TraceId { get; set; }
-    public string SessionId { get; set; }
+    public string UniqueId { get; set; } // UID
+    public string SessionId { get; set; } // SID
     public string UserName { get; set; }
     public IPAddress UserIp { get; set; }
     public string Language { get; set; }
