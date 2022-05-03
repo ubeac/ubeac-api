96c5128009fdde72d4ef9ceb86b58cd7601eadb9
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-05-03 17:12:58 +0430

    Refactor HistoryEntity ( Remove interfaces and Put one class )

--- src/Common/uBeac.Core.Common/Entities/HistoryEntity.cs
+++ src/Common/uBeac.Core.Common/Entities/HistoryEntity.cs
@@ -1,27 +1,14 @@
 ﻿namespace uBeac;
 
-public interface IHistoryEntity<TKey> : IEntity<TKey>
-    where TKey : IEquatable<TKey>
+public class HistoryEntity<TData> : Entity
 {
-    object Data { get; set; }
-    string ActionName { get; set; }
-    DateTime CreatedAt { get; set; }
-    IApplicationContext Context { get; set; }
-}
-
-public interface IHistoryEntity : IEntity, IHistoryEntity<Guid>
-{
-}
-
-public class HistoryEntity<TKey> : Entity<TKey>, IHistoryEntity<TKey>
-    where TKey : IEquatable<TKey>
-{
-    public virtual object Data { get; set; }
+    public virtual TData Data { get; set; }
+    public virtual string DataId { get; set; }
     public virtual string ActionName { get; set; }
     public virtual DateTime CreatedAt { get; set; }
     public virtual IApplicationContext Context { get; set; }
 }
 
-public class HistoryEntity : HistoryEntity<Guid>, IHistoryEntity
+public class HistoryEntity : HistoryEntity<object>
 {
 }
