0a2bebaaccb3d63e634dfb8a8162824557433764
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-05-03 17:14:11 +0430

    Remove unused extension methods from History.MongoDB

--- src/Repository/uBeac.Core.Repositories.History.MongoDB/Extensions.cs
+++ src/Repository/uBeac.Core.Repositories.History.MongoDB/Extensions.cs
@@ -1,22 +1,10 @@
-﻿using uBeac;
-using uBeac.Repositories.History.MongoDB;
+﻿using uBeac.Repositories.History.MongoDB;
 using uBeac.Repositories.MongoDB;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
 public static class Extensions
 {
-    public static IHistoryRepositoryRegistration UsingMongoDb<TKey, THistory, TContext>(this IHistoryRegistration registration)
-        where TKey : IEquatable<TKey>
-        where THistory : class, IHistoryEntity<TKey>, new()
-        where TContext : IMongoDBContext
-        => registration.Using<MongoHistoryRepository<TKey, THistory, TContext>>();
-
-    public static IHistoryRepositoryRegistration UsingMongoDb<THistory, TContext>(this IHistoryRegistration registration)
-        where THistory : class, IHistoryEntity, new()
-        where TContext : IMongoDBContext
-        => registration.Using<MongoHistoryRepository<THistory, TContext>>();
-
     public static IHistoryRepositoryRegistration UsingMongoDb<TContext>(this IHistoryRegistration registration)
         where TContext : IMongoDBContext
         => registration.Using<MongoHistoryRepository<TContext>>();
