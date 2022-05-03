31e3ffa9a892cf80137a15d6f0d62cd23d1c2ad3
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-05-03 17:25:32 +0430

    Add default value to cancellation token ( Correction )

--- src/Repository/uBeac.Core.Repositories.History.Abstractions/IHistoryRepository.cs
+++ src/Repository/uBeac.Core.Repositories.History.Abstractions/IHistoryRepository.cs
@@ -5,5 +5,5 @@ public interface IHistoryRepository : IRepository
     Task Add<TData>(TData data, string dataId = null, string actionName = "None", IApplicationContext context = null, CancellationToken cancellationToken = default);
 
     Task<IEnumerable<HistoryEntity<TData>>> GetAll<TData>(string dataId, CancellationToken cancellationToken = default);
-    Task<IEnumerable<HistoryEntity>> GetAll(Type dataType, string dataId, CancellationToken cancellationToken);
+    Task<IEnumerable<HistoryEntity>> GetAll(Type dataType, string dataId, CancellationToken cancellationToken = default);
 }
