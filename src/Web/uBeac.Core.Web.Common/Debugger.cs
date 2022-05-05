cf8f72cf49bb28e7644b64ac8020793786563053
Amir Pournasserian <ap@momentaj.com>
2022-05-05 17:18:35 -0400

    Debugger - cleanup

--- src/Web/uBeac.Core.Web.Common/Debugger.cs
+++ src/Web/uBeac.Core.Web.Common/Debugger.cs
@@ -4,20 +4,19 @@ namespace uBeac.Web;
 
 public class Debugger : IDebugger
 {
-    private readonly HttpContext _context;
     private const string ITEMS_KEY = "internalDebug";
+    private readonly List<object> items;
 
     public Debugger(IHttpContextAccessor accessor)
     {
-        _context = accessor.HttpContext;
+        items = new List<object>();
+        accessor.HttpContext.Items[ITEMS_KEY] = items;
     }
 
     public void Add(object value)
     {
-        _context.Items[ITEMS_KEY] ??= new List<object>();
-        var debugList = _context.Items[ITEMS_KEY] as List<object>;
-        debugList!.Add(value);
+        items.Add(value);
     }
 
-    public List<object> GetValues() => _context.Items[ITEMS_KEY] as List<object> ?? new List<object>();
+    public List<object> GetValues() => items;
 }
