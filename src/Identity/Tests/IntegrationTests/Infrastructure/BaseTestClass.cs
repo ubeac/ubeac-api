fdc589e227f510a0b75766403b000973d81803ec
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-05-08 13:12:38 +0430

    Refactor unit tests of ApplicationContext

--- src/Identity/Tests/IntegrationTests/Infrastructure/BaseTestClass.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace IntegrationTests;
-
-public class BaseTestClass : PriorityOrderedTests
-{
-}
--- src/Web/UnitTests/uBeac.Core.Web.Common.UnitTests/ApplicationContext.cs
+++ src/Web/UnitTests/uBeac.Core.Web.Common.UnitTests/ApplicationContext.cs
@@ -9,46 +9,62 @@ namespace uBeac.Web;
 
 public class ApplicationContextTests
 {
-    [Fact]
-    public void ApplicationContext_Should_Be_Constructed_Using_HttpContextAccessor()
-    {
-        const string traceId = "ThisIsTestTraceId";
-        const string sessionId = traceId;
-        const string userName = "ThisIsTestUserName";
-        const string userIp = "127.0.0.1";
-        const string language = "fa-IR";
+    private const string TraceId = "ThisIsTestTraceId";
+    private const string SessionId = TraceId;
+    private const string UserName = "ThisIsTestUserName";
+    private const string UserIp = "127.0.0.1";
+    private const string Language = "fa-IR";
 
+    private readonly ApplicationContext _applicationContext;
+
+    public ApplicationContextTests()
+    {
         var requestHeaders = new HeaderDictionary
         {
-            { "Accept-Language", language }
+            { "Accept-Language", Language }
         };
 
         var httpRequestMock = new Mock<HttpRequest>();
         httpRequestMock.Setup(httpRequest => httpRequest.Headers).Returns(requestHeaders);
 
         var identityMock = new Mock<IIdentity>();
-        identityMock.Setup(identity => identity.Name).Returns(userName);
+        identityMock.Setup(identity => identity.Name).Returns(UserName);
 
         var claimsPrincipalMock = new Mock<ClaimsPrincipal>();
         claimsPrincipalMock.Setup(claimsPrincipal => claimsPrincipal.Identity).Returns(identityMock.Object);
-        
+
         var connectionInfoMock = new Mock<ConnectionInfo>();
-        connectionInfoMock.Setup(connectionInfo => connectionInfo.RemoteIpAddress).Returns(IPAddress.Parse(userIp));
+        connectionInfoMock.Setup(connectionInfo => connectionInfo.RemoteIpAddress).Returns(IPAddress.Parse(UserIp));
 
         var httpContextMock = new Mock<HttpContext>();
         httpContextMock.Setup(context => context.Request).Returns(httpRequestMock.Object);
-        httpContextMock.Setup(context => context.TraceIdentifier).Returns(traceId);
+        httpContextMock.Setup(context => context.TraceIdentifier).Returns(TraceId);
         httpContextMock.Setup(context => context.User).Returns(claimsPrincipalMock.Object);
         httpContextMock.Setup(context => context.Connection).Returns(connectionInfoMock.Object);
 
         var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
         httpContextAccessorMock.Setup(accessor => accessor.HttpContext).Returns(httpContextMock.Object);
 
-        var appContext = new ApplicationContext(httpContextAccessorMock.Object);
-        Assert.Equal(traceId, appContext.TraceId);
-        Assert.Equal(sessionId, appContext.SessionId);
-        Assert.Equal(userName, appContext.UserName);
-        Assert.Equal(userIp, appContext.UserIp);
-        Assert.Equal(language, appContext.Language);
+        _applicationContext = new ApplicationContext(httpContextAccessorMock.Object);
+    }
+
+    [Fact]
+    public void Constructor_PropertiesShouldNotBeNull()
+    {
+        Assert.NotNull( _applicationContext.TraceId);
+        Assert.NotNull(_applicationContext.SessionId);
+        Assert.NotNull(_applicationContext.UserName);
+        Assert.NotNull(_applicationContext.UserIp);
+        Assert.NotNull(_applicationContext.Language);
+    }
+
+    [Fact]
+    public void Constructor_PropertiesShouldEqualsWithInputs()
+    {
+        Assert.Equal(TraceId, _applicationContext.TraceId);
+        Assert.Equal(SessionId, _applicationContext.SessionId);
+        Assert.Equal(UserName, _applicationContext.UserName);
+        Assert.Equal(UserIp, _applicationContext.UserIp);
+        Assert.Equal(Language, _applicationContext.Language);
     }
 }
