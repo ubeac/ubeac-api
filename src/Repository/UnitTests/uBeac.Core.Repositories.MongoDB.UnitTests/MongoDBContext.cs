39dd444f97d4b5a699b8f2871357a3a83b322723
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-05-08 15:21:12 +0430

    Refactor unit tests of MongoDBContext

--- src/Repository/UnitTests/uBeac.Core.Repositories.MongoDB.UnitTests/MongoDBContext.cs
+++ src/Repository/UnitTests/uBeac.Core.Repositories.MongoDB.UnitTests/MongoDBContext.cs
@@ -1,20 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using Xunit;
+﻿using Xunit;
 
 namespace uBeac.Repositories.MongoDB;
 
 public class MongoDBContextTests
 {
-    [Fact]
-    public void Database_Should_Not_Be_Null_After_Context_Construction()
+    private const string ConnectionString = "mongodb://localhost:27017/test-db";
+
+    private readonly MongoDBContext _mongoDbContext;
+
+    public MongoDBContextTests()
     {
-        var mongoDbOptions = new MongoDBOptions($"mongodb://localhost:27017/test-db{new Random().Next()}");
+        var mongoDbOptions = new MongoDBOptions(ConnectionString);
         var bsonSerializationOptions = new BsonSerializationOptions();
 
-        var context = new MongoDBContext(mongoDbOptions, bsonSerializationOptions);
+        _mongoDbContext = new MongoDBContext(mongoDbOptions, bsonSerializationOptions);
+    }
 
-        Assert.NotNull(context);
-        Assert.NotNull(context.Database);
+    [Fact]
+    public void Constructor_DatabaseShouldNotBeNull()
+    {
+        Assert.NotNull(_mongoDbContext);
+        Assert.NotNull(_mongoDbContext.Database);
     }
 }
