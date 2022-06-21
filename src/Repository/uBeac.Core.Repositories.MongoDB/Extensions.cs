ae32cd3f88ae3694ee0214c139464868ba68f384
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-06-21 18:31:42 +0430

    Remove "Core.MongoDB" + Modify dependencies of repository libraries

--- src/Repository/uBeac.Core.Repositories.MongoDB/Extensions.cs
+++ src/Repository/uBeac.Core.Repositories.MongoDB/Extensions.cs
@@ -1,10 +1,49 @@
-﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using uBeac;
 using uBeac.Repositories;
+using uBeac.Repositories.MongoDB;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
 public static class RepositoryExtensions
 {
+    public static IServiceCollection AddMongo<TMongoDbContext>(this IServiceCollection services, string connectionString)
+        where TMongoDbContext : class, IMongoDBContext
+    {
+        services.TryAddScoped(provider =>
+        {
+            var configuration = provider.GetService<IConfiguration>();
+            var connString = configuration.GetConnectionString(connectionString);
+            return new MongoDBOptions<TMongoDbContext>(connString);
+        });
+
+        services.TryAddScoped<TMongoDbContext>();
+        services.TryAddScoped<IMongoDBContext, TMongoDbContext>();
+
+        services.TryAddScoped(provider =>
+        {
+            var appContextType = provider.CreateScope().ServiceProvider.GetRequiredService<IApplicationContext>().GetType();
+
+            return new BsonSerializationOptions
+            {
+                Serializers = new Dictionary<Type, IBsonSerializer>
+                {
+                    { typeof(Guid), new GuidSerializer(GuidRepresentation.Standard) },
+                    { typeof(decimal), new DecimalSerializer(BsonType.Decimal128) },
+                    { typeof(decimal?), new NullableSerializer<decimal>(new DecimalSerializer(BsonType.Decimal128)) },
+                    { typeof(IApplicationContext), new AppContextBsonSerializer(appContextType) }
+                },
+                GuidRepresentationMode = GuidRepresentationMode.V3
+            };
+        });
+
+        return services;
+    }
+
     public static IServiceCollection AddRepository<TInterface, TImplementation>(this IServiceCollection services)
         where TInterface : IRepository
         where TImplementation : class, IRepository
