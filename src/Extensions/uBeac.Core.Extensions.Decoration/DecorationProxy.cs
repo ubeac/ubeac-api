baabfe03234b867b36500ddf5a93202a01318359
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-06-23 17:15:50 +0430

    Initialized "uBeac.Extensions.Decoration"

---
+++ src/Extensions/uBeac.Core.Extensions.Decoration/DecorationProxy.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+
+namespace uBeac.Extensions.Decoration;
+
+public class DecorationProxy<TTarget, TDecorator> : DispatchProxy
+{
+    public TTarget Target { get; set; }
+    public TDecorator Decorator { get; set; }
+
+    public static TTarget Decorate(TTarget target, TDecorator decorator)
+    {
+        var proxy = Create<TTarget, DecorationProxy<TTarget, TDecorator>>();
+        (proxy as DecorationProxy<TTarget, TDecorator>)!.Target = target;
+        (proxy as DecorationProxy<TTarget, TDecorator>)!.Decorator = decorator;
+        return proxy;
+    }
+
+    protected override object Invoke(MethodInfo targetMethod, object[] args)
+    {
+        if (typeof(TDecorator).GetMethods().Any(method => AreMethodEquals(method, targetMethod)))
+        {
+            return targetMethod.Invoke(Decorator, args);
+        }
+
+        return targetMethod.Invoke(Target, args);
+    }
+
+    private static bool AreMethodEquals(MethodInfo left, MethodInfo right)
+    {
+        if (left.Equals(right)) return true;
+        if (left.Name != right.Name) return false;
+        if (left.MemberType != right.MemberType) return false;
+        if (left.ReturnType != right.ReturnType) return false;
+
+        var leftParams = left.GetParameters();
+        var rightParams = right.GetParameters();
+
+        if (leftParams.Length != rightParams.Length) return false;
+        return !leftParams.Where((t, i) => t.ParameterType != rightParams[i].ParameterType).Any();
+    }
+}
---
+++ src/Extensions/uBeac.Core.Extensions.Decoration/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using uBeac.Extensions.Decoration;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDecorator<TTargetInterface, TTargetImplementation, TDecorator>(this IServiceCollection services)
+        where TTargetImplementation : class, TTargetInterface
+        where TDecorator : class
+    {
+        return services.Replace(new ServiceDescriptor(typeof(TTargetInterface), serviceProvider =>
+        {
+            var target = ActivatorUtilities.GetServiceOrCreateInstance<TTargetImplementation>(serviceProvider);
+            var decorator = ActivatorUtilities.CreateInstance<TDecorator>(serviceProvider, target);
+            return DecorationProxy<TTargetInterface, TDecorator>.Decorate(target, decorator);
+        }, ServiceLifetime.Scoped));
+    }
+}
