a21527b2f57825c6843684401f2c0d5bda6ec9fe
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-06-26 13:42:18 +0430

    Added IFileProvider

---
+++ src/FileManagement/uBeac.Core.Providers.FileManagement.Abstractions/IFileProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace uBeac.Providers.FileManagement;
+
+public interface IFileProvider
+{
+    Task Create(FileStream fileStream, string fileName, CancellationToken cancellationToken = default);
+    Task<FileStream> Get(string fileName, CancellationToken cancellationToken = default);
+}
