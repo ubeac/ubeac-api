a0bfef9c0c401d6eb5709ded27e676a40dd7b07f
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-06-30 14:17:51 +0430

    Test - Added FilesController

---
+++ src/Identity/Tests/API/Controllers/FilesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using uBeac.FileManagement;
+using uBeac.Web;
+
+namespace API;
+
+public class FilesController : BaseController
+{
+    protected readonly IFileManager FileManager;
+
+    public FilesController(IFileManager fileManager)
+    {
+        FileManager = fileManager;
+    }
+
+    [HttpPost]
+    public async Task Avatar([FromForm] IFormFile file, CancellationToken cancellationToken = default)
+    {
+        await using var stream = file.OpenReadStream();
+        await FileManager.Create(new CreateFileRequest
+        {
+            Stream = stream,
+            Category = "Avatars",
+            Extension = Path.GetExtension(file.FileName)
+        }, cancellationToken);
+    }
+}
