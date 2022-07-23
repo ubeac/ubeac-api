ccca373611b5985b2579422f03ec0a1be13ef45b
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-07-23 17:32:38 +0430

    Use MongoDBContext in MongoDbHttpLogRepository

--- src/Logging/uBeac.Core.Web.Logging.MongoDB/Repository.cs
+++ src/Logging/uBeac.Core.Web.Logging.MongoDB/Repository.cs
@@ -1,32 +1,25 @@
 ﻿using MongoDB.Driver;
+using uBeac.Repositories.MongoDB;
 
 namespace uBeac.Web.Logging.MongoDB;
 
-public class MongoDbHttpLogRepository : IHttpLogRepository
+public class MongoDbHttpLogRepository<TContext> : IHttpLogRepository
+    where TContext : IMongoDBContext
 {
+    protected readonly TContext Context;
     protected readonly MongoDbHttpLogOptions Options;
 
-    public MongoDbHttpLogRepository(MongoDbHttpLogOptions options)
+    public MongoDbHttpLogRepository(TContext context, MongoDbHttpLogOptions options)
     {
+        Context = context;
         Options = options;
     }
 
     public async Task Create(HttpLog log, CancellationToken cancellationToken = default)
     {
-        var connectionString = Options.ConnectionString;
         var collectionName = Options.GetCollectionName(log.StatusCode);
-
-        var database = GetDatabase(connectionString);
-        var collection = GetCollection(database, collectionName);
-
+        var collection = GetCollection(Context.Database, collectionName);
         await collection.InsertOneAsync(log, new InsertOneOptions(), cancellationToken);
-    }
-
-    protected virtual IMongoDatabase GetDatabase(string connectionString)
-    {
-        var mongoUrl = new MongoUrl(connectionString);
-        var client = new MongoClient(mongoUrl);
-        return client.GetDatabase(mongoUrl.DatabaseName);
     }
 
     protected virtual IMongoCollection<HttpLog> GetCollection(IMongoDatabase database, string collectionName)
